d2dda36715ff068dd916724456a6ef755037cfb0
samuelsold <samuelbarrios984@gmail.com>
2020-01-09 12:04:28 -0300

    Initial commit

---
+++ Autos.java
@@ -0,0 +1,8 @@
+public class Autos{
+	public static void main(String args[]){
+	System.out.println("Peugeot 208 5 Puerta Precio=250.000 pesos");
+	System.out.println("Peugeot 206 4 Puerta Precio=250.000 pesos");
+	System.out.println("Yamaha YBR cilindrada:160c Precio=250.000 pesos");
+	System.out.println("Honda titan cilindrada: 125c Precio=60.000 pesos");
+	}
+}
